82a91d7ac603fad34c5b4de0fdb02611b3deb6a8
shreyash-01 <singhshreyash0075@gmail.com>
2023-03-07 00:37:09 +0530

    Added new feature which displays humidity, chance of rainfall, pressure. windspeed. Added 4 new icons for the same. Initialised all the current views in the java file.

--- app/src/main/java/com/shreyash/weatherapp/MainActivity.java
+++ app/src/main/java/com/shreyash/weatherapp/MainActivity.java
@@ -3,14 +3,36 @@ package com.shreyash.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView temperature;
+    private TextView minMaxTemperature;
+    private TextView description;
+    private TextView feelsLikeTemperature;
+    private TextView chanceOfRainfallDynamic;
+    private TextView humidityDynamic;
+    private TextView pressureDynamic;
+    private TextView windSpeedDynamic;
+    private TextView cityName;
+    private TextView date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        temperature = findViewById(R.id.temperature);
+        minMaxTemperature = findViewById(R.id.minMaxTemperature);
+        description = findViewById(R.id.description);
+        feelsLikeTemperature = findViewById(R.id.feelsLikeTemperature);
+        chanceOfRainfallDynamic = findViewById(R.id.chanceOfRainfallDynamic);
+        humidityDynamic = findViewById(R.id.humidityDynamic);
+        pressureDynamic = findViewById(R.id.pressureDynamic);
+        windSpeedDynamic = findViewById(R.id.windSpeedDynamic);
+        cityName = findViewById(R.id.cityName);
+        date = findViewById(R.id.date);
+
 
     }
 }
